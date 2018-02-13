ebce474418691685325001e460d30c34caf1f011
Micael Cruz <micaelcruz89@gmail.com>
2018-02-13 13:40:09 -0200

    Adiciona roteador

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,12 @@
 <template>
   <div>
-    <section class="hero is-primary">
-      <div class="hero-body">
-          <div class="container">
-            <p class="title is-1">Pelada</p>
-            <p class="subtitle is-3">{{ game.day }}</p>
-          </div>
-        </div>
-      </section>
-
-      <div class="demo">
-        <table class="table is-bordered">
-            <thead>
-              <tr>
-                  <th>Lista de Confirmados</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="player of game.players"> 
-                <td>{{ player.nickName }}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
-        <div class="demo">
-          <table class="table is-bordered is-responsive">
-            <thead>
-              <tr>
-                  <th>Lista de Espera</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="player of game.waitingList"> 
-                <td>{{ player.nickName }}</td>
-              </tr>
-            </tbody>
-          </table>
-      </div>
-
-      
-
-      
-
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return{
-      game: {}
-    }
-  },
-  created () {
-    this.$http.get('http://localhost:8080/pelada/list')
-      .then(result => result.json())
-      .then(function(game){
-        this.game = game;
-        this.game.day = new Date(game.day).toLocaleString();
-        console.log(game);
-      });
-  }
+  
 }
 </script>
 
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+import Pelada from './components/Pelada.vue';
+
+export const routes = [
+    {path: '/', component: Pelada}
+];
